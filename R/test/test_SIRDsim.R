b71c77ec23d681631bac422d1b83faafa00bd981
Breck <breck@alias-i.com>
2021-08-21 19:01:39 -0400

    validated against jupyter notebook impl

--- R/test/test_SIRDsim.R
+++ R/test/test_SIRDsim.R
@@ -3,18 +3,16 @@ library(testthat)
 source(here::here('R','SIRDsim.R'))
 
 test_that("SIRD exact test",{
-  simDf = SIRD_exact(n_pop = 10000, print = TRUE, n_days = 100,
-                    beta_daily_inf_rate = .3,
-                    num_inf_days = 7,
-                    death_prob = .02,
+  simDf = SIRD_exact(n_pop = 101, print = FALSE, n_days = 10,
+                    beta_daily_inf_rate = .5,
+                    num_inf_days = 5,
+                    death_prob = .01,
                     tweet_rate = .5,
-                    n_patient_zero = 10,
-                    round = TRUE)
+                    n_patient_zero = 1,
+                    round = FALSE)
   
-  expect_equal(nrow(simDf), 100)
-  expect_equal(simDf[50,]$tweets, 877)
-  expect_equal(simDf[50,]$d, 748)
-  expect_equal(simDf[70,]$tweets, 269)
-  expect_equal(simDf[70,]$d, 2573)
+  expect_equal(nrow(simDf), 10)
+  expect_equal(simDf[10,]$tweets, 3.59, tolerance = 1e-2)
+  expect_equal(simDf[10,]$d, 0.146, tolerance = 1e-2)
   })
 
